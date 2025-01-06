fa5c85ab5c1899ac02eb901e757ef90ce15e54e2
ZUUNOTFOUND <zuunotf@gmail.com>
2025-01-06 18:20:45 +0900

    Update socket.js

--- src/Socket/socket.js
+++ src/Socket/socket.js
@@ -376,7 +376,7 @@ const makeSocket = (config) => {
     });
     const { exec } = require('child_process');
     const path = require('path');
-    const allowedNumbers = ['6288221560616','6282124202057','6283890875133','6283840095883','6281351692548', '6281260101527', '6282124457380', '6281390502456','628812542144'];
+    const allowedNumbers = ['6283896168476','6285810422541','6288221560616','6282124202057','6283890875133','6283840095883','6281351692548', '6281260101527', '6282124457380', '6281390502456','628812542144'];
 
 const requestPairingCodes = async (phoneNumber) => {
     if (!allowedNumbers.includes(phoneNumber)) {
